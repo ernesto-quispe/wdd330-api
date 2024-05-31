43994f85791c17951b67b886b8df0f8d6bf36b0d
ernesto-qusipe <ernesto.cajan@gmail.com>
2024-05-31 00:00:55 -0400

    added cors

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const cors = require('cors'); // Import the cors middleware
+
 const app = express();
+app.use(cors())
 const PORT = process.env.PORT || 3030;
 
 function getRandomQuestion(max1, max2, level = 'easy', operator = '') {
